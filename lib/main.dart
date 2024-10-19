4943a7dfc72a152a4706bca94c917c3a59ae2d52
Sabbir Hossain <sabbir.hossain.se62@gmail.com>
2024-10-19 18:59:02 +0600

    Drawer added

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -23,6 +23,13 @@ class MyApp extends StatelessWidget {
 
 class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
+
+  mySnakeBar(message, context){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text(message))
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,11 +41,48 @@ class HomeActivity extends StatelessWidget {
         elevation: 6,
         backgroundColor: Colors.teal,
         actions: [
-          IconButton(onPressed: (){}, icon: const Icon(Icons.search)),
-          IconButton(onPressed: (){}, icon: const Icon(Icons.comment)),
-          IconButton(onPressed: (){}, icon: const Icon(Icons.email))
+          IconButton(onPressed: (){mySnakeBar("Hello, I am Search", context);}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: (){mySnakeBar("Hello, I am Comment", context);}, icon: const Icon(Icons.comment)),
+          IconButton(onPressed: (){mySnakeBar("Hello, I am Email", context);}, icon: const Icon(Icons.email))
         ],
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        onPressed: (){
+          mySnakeBar("I am floating action button", context);
+        },
+        backgroundColor: Colors.teal,
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Contact"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+        onTap: (int index){
+          if(index == 0){
+            mySnakeBar("I am home", context);
+          }
+          if(index == 1){
+          mySnakeBar("I am contact", context);
+          }
+          if(index == 2){
+            mySnakeBar("I am profile", context);
+          }
+        },
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            DrawerHeader(child: Text("Sabbir")),
+            ListTile(title: Text("Home")),
+            ListTile(title: Text("Home")),
+            ListTile(title: Text("Home"))
+          ],
+        ),
+      ),
     );
   }
 }
